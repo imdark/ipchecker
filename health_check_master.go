e0cf94fa18657a221d013fc1fc44305c0a112234
Michael Kris <mikesgoacademy@gmail.com>
2017-12-16 23:48:40 -0800

    figured out config reading and docker compose

--- health_check_master.go
+++ health_check_master.go
@@ -94,6 +94,7 @@ func main() {
 
 	for i := 0; i < agentUrlsLen; i++ {
 		result := <-reports
+		fmt.Printf("%+v", result)
 		if result.Err != nil {
 			fmt.Println("Error during connecting to client ", result.Err)
 			continue
